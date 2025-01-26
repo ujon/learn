8763fc349d12de23da67fe39dead83eae2939c0f
ujon <ujon94@gmail.com>
2025-01-25 16:53:07 -0800

    Add media project

---
+++ rust/projects/README.md
@@ -0,0 +1,7 @@
+# Projects
+
+## Order of Projects
+
+1. deck
+2. bank
+3. media
---
+++ rust/projects/media/src/main.rs
@@ -0,0 +1,16 @@
+struct Book {
+    title: String,
+    author: String,
+}
+
+struct Media {
+    title: String,
+    director: String,
+}
+
+struct AudioBook {
+    title: String,
+}
+
+fn main() {
+}
